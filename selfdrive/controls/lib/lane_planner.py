19c640585dc7452e55c6c0c970ff351a3ac3ea26
azure93 <azure93@gmail.com>
2020-09-05 08:28:53 +0900

    Add files via upload
    lqr fix / lane planner

--- selfdrive/controls/lib/lane_planner.py
+++ selfdrive/controls/lib/lane_planner.py
@@ -4,6 +4,9 @@ from cereal import log
 
 CAMERA_OFFSET = 0.06  # m from center car to camera
 
+#zorrobyte
+def mean(numbers): 
+     return float(sum(numbers)) / max(len(numbers), 1) 
 
 def compute_path_pinv(l=50):
   deg = 3
@@ -41,6 +44,11 @@ def calc_d_poly(l_poly, r_poly, p_poly, l_prob, r_prob, lane_width, v_ego):
 
   lr_prob = l_prob + r_prob - l_prob * r_prob
 
+  #if lr_prob > 0.80 :
+  #  lr_prob = max(0.95,lr_prob)
+  lr_prob = min(lr_prob * 1.3, 1.0) # 차선 30% 추가 가중치
+
+
   d_poly_lane = (l_prob * path_from_left_lane + r_prob * path_from_right_lane) / (l_prob + r_prob + 0.0001)
   return lr_prob * d_poly_lane + (1.0 - lr_prob) * p_poly
 
@@ -52,9 +60,13 @@ class LanePlanner():
     self.p_poly = [0., 0., 0., 0.]
     self.d_poly = [0., 0., 0., 0.]
 
-    self.lane_width_estimate = 3.7
-    self.lane_width_certainty = 1.0
-    self.lane_width = 3.7
+    #self.lane_width_estimate = 3.7
+    #self.lane_width_certainty = 1.0
+    #self.lane_width = 3.7
+    #zorro
+    self.lane_width = 2.85
+    self.readings = []
+    self.frame = 0
 
     self.l_prob = 0.
     self.r_prob = 0.
@@ -87,12 +99,27 @@ class LanePlanner():
     self.r_poly[3] += CAMERA_OFFSET
 
     # Find current lanewidth
-    self.lane_width_certainty += 0.05 * (self.l_prob * self.r_prob - self.lane_width_certainty)
-    current_lane_width = abs(self.l_poly[3] - self.r_poly[3])
-    self.lane_width_estimate += 0.005 * (current_lane_width - self.lane_width_estimate)
-    speed_lane_width = interp(v_ego, [0., 31.], [2.8, 3.5])
-    self.lane_width = self.lane_width_certainty * self.lane_width_estimate + \
-                      (1 - self.lane_width_certainty) * speed_lane_width
+    #self.lane_width_certainty += 0.05 * (self.l_prob * self.r_prob - self.lane_width_certainty)
+    #current_lane_width = abs(self.l_poly[3] - self.r_poly[3])
+    #self.lane_width_estimate += 0.005 * (current_lane_width - self.lane_width_estimate)
+    #speed_lane_width = interp(v_ego, [0., 31.], [2.8, 3.5])
+    #self.lane_width = self.lane_width_certainty * self.lane_width_estimate + \
+    #                  (1 - self.lane_width_certainty) * speed_lane_width
+
+    #zorrobyte code
+    if self.l_prob > 0.49 and self.r_prob > 0.49:
+      self.frame += 1
+      if self.frame % 20 == 0:
+        self.frame = 0
+        current_lane_width = sorted((2.5, abs(self.l_poly[3] - self.r_poly[3]), 3.5))[1]
+        max_samples = 30
+        self.readings.append(current_lane_width)
+        self.lane_width = mean(self.readings)
+        if len(self.readings) == max_samples:
+          self.readings.pop(0)
+    if abs(self.l_poly[3] - self.r_poly[3]) > self.lane_width:
+      self.r_prob = self.r_prob / interp(self.l_prob, [0, 1], [1, 3])
+    #end of zorrobyte
 
     self.d_poly = calc_d_poly(self.l_poly, self.r_poly, self.p_poly, self.l_prob, self.r_prob, self.lane_width, v_ego)
 
--- selfdrive/controls/lib/latcontrol_lqr.py
+++ selfdrive/controls/lib/latcontrol_lqr.py
@@ -1,3 +1,4 @@
+from selfdrive.ntune import nTune
 import numpy as np
 from selfdrive.controls.lib.drive_helpers import get_steer_max
 from common.numpy_fast import clip
@@ -25,6 +26,7 @@ class LatControlLQR():
     self.sat_limit = CP.steerLimitTimer
 
     self.reset()
+    self.tune = nTune(CP, self) # Added for utune
 
   def reset(self):
     self.i_lqr = 0.0
@@ -44,10 +46,14 @@ class LatControlLQR():
     return self.sat_count > self.sat_limit
 
   def update(self, active, v_ego, angle_steers, angle_steers_rate, eps_torque, steer_override, rate_limited, CP, path_plan):
+    self.tune.check() # Added for utune
     lqr_log = log.ControlsState.LateralLQRState.new_message()
 
     steers_max = get_steer_max(CP, v_ego)
-    torque_scale = (0.45 + v_ego / 60.0)**2  # Scale actuator model with speed
+    #torque_scale = (0.45 + v_ego / 60.0) ** 2  # Scale actuator model with speed
+    torque_scale = (0.55 + v_ego / 86.0) ** 2
+    #torque_scale = min(torque_scale, 0.616)
+    torque_scale = min(torque_scale, 0.7)
 
     # Subtract offset. Zero angle should correspond to zero torque
     self.angle_steers_des = path_plan.angleSteers - path_plan.angleOffset
