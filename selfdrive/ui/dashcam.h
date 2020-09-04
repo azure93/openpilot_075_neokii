a2ddf11700d1f5dd6264e0c40c9c7976b740951e
azure93 <azure93@gmail.com>
2020-09-04 19:02:02 +0900

    Update dashcam.h
    add recording stop condition.

--- selfdrive/ui/dashcam.h
+++ selfdrive/ui/dashcam.h
@@ -336,12 +336,15 @@ void dashcam( UIState *s, int touch_x, int touch_y ) {
   if (!s->vision_connected) {
     // Assume car is not in drive so stop recording
     stop_capture();
+  } else if (!s->scene.engageable) {
+    stop_capture();
   }
+
 //  if (s->scene.v_ego > 2.1 && captureState == CAPTURE_STATE_NOT_CAPTURING && !s->scene.engaged) {
 //    start_capture();
 //  } else if (s->scene.v_ego < 1.5 && !s->scene.engaged) {
-  if (s->scene.v_ego < 1.5 && !s->scene.engaged) {
-    stop_capture();
-  }
+//  if (s->scene.v_ego < 1.5 && !s->scene.engaged) {
+//    stop_capture();
+//  }
   s->scene.recording = (captureState != CAPTURE_STATE_NOT_CAPTURING);
 }
